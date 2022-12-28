ca53544f7d6d93436af05b907f59b70aaeaa2a34
vrbdas <vrbdas@gmail.com>
2022-12-28 20:59:17 +0600

    28.12.2022

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,22 @@
-/* $(document).ready(function(){
-	$('.carousel__slider').slick({
-		speed: 1000,
-		prevArrow: '<button type="button" class="slick-prev"><img src="icons/prevArrow.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="icons/nextArrow.png"></button>',
-		responsive: [
-			{
-			breakpoint: 992,
-			settings: {
-				dots: true,
-				arrows: false
-			  }
-			}
-		  ]
-	  });
-  }); */
-
-
-  
 const slider = tns({
 	container: '.carousel__slider',
 	items: 1,
 	slideBy: 'page',
-	autoplay: false
+	autoplay: false,
+	controls: false,
+	navPosition: 'bottom',
+	mouseDrag: true,
+	nav: true,
+	responsive: {
+		1200: {
+			nav: false
+		},
+	  }
 });
 
 document.querySelector('.prev').onclick = function () {
 	slider.goTo('prev');
-  };
+};
+document.querySelector('.next').onclick = function () {
+	slider.goTo('next');
+};
