d5687c9a890c8c91181a6ed3dbd1f4e222ae4ea9
vrbdas <vrbdas@gmail.com>
2023-01-20 17:15:05 +0600

    работает отправка почты но отправляет пустые тоже

--- src/js/script.js
+++ src/js/script.js
@@ -124,7 +124,7 @@ document.querySelector(".next").onclick = function () {
 		}
 	});
 
-	$("a[href^='#']").click(function () {
+	$("a[class='pageup']").click(function () {
 		const _href = $(this).attr("href");
 		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
 		return false;
